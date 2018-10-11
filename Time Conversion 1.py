03b991d25de55f11393865817654928d52013bf6
vishal sharma <vishalsh533@gmail.com>
2018-10-11 16:22:51 +0530

    Update Time Conversion 1.py

--- Time Conversion 1.py
+++ Time Conversion 1.py
@@ -1,9 +1,9 @@
 # submitted by shareef
 from datetime import datetime
-print('input in the form of "HH:MM:SS AM or PM"')
+print('input in the form of "HH:MM:SSAM or PM"')
 print('\n')
 t= str(input(''))
-t1 = datetime.strptime(t, "%I:%M:%S %p")
+t1 = datetime.strptime(t, "%I:%M:%S%p")
 t2 = datetime.strftime(t1, "%H:%M:%S")
 print(t2)
     
