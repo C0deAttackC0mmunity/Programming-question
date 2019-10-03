7825392d4c04aa630ad6bbb0c5a4c97a45815150
Kapil Bansal <42766576+KapilBansal320@users.noreply.github.com>
2019-10-03 21:49:43 +0530

    Update Anagrams.py
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/anagrams-651/

--- Anagrams.py
+++ Anagrams.py
@@ -1,3 +1,7 @@
+#Given two strings, a and b , that may or may not be of the same length,
+#determine the minimum number of character deletions required to make a and b anagrams.
+#Any characters can be deleted from either of the strings.
+
 for i in range(int(input())):
     l1=[i for i in input().sort()]
     l2=[i for i in input().sort()]
