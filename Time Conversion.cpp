8ad7923571f3f8334056bba61f09e95ce5daaa10
Kowsihan <56033238+Kowsihan@users.noreply.github.com>
2019-10-12 12:58:49 +0530

    Update Time Conversion.cpp

--- Time Conversion.cpp
+++ Time Conversion.cpp
@@ -1,9 +1,12 @@
 /* submitted by manish*/
 # include <bits/stdc++.h>
+#include <string>
+
 using namespace std;
 int main()
 {
-    string st;
+    string st,s;
+    int b;
     cin>>st;
     if(st[8] == 'A')
     {
@@ -13,69 +16,22 @@ int main()
             st[1] = '0';
         }
     }
-
-    else
-    {
-        if(st[0] == '0' and st[1] == '1')
-            {
-                st[0] = '1';
-                st[1] = '3';
+    else{
+        if(st[0]=='1' && st[1]=='2')[
+            st[0]='1';
+            st[1]='2';
             }
-
-        else if(st[0] == '0' and st[1] == '2')
-            {
-                st[0] = '1';
-                st[1] = '4';
-            }
-        else if(st[0] == '0' and st[1] == '3')
-            {
-                st[0] = '1';
-                st[1] = '5';
-            }
-        else if(st[0] == '0' and st[1] == '4')
-            {
-                st[0] = '1';
-                st[1] = '6';
-            }
-        else if(st[0] == '0' and st[1] == '5')
-            {
-                st[0] = '1';
-                st[1] = '7';
-            }
-        else if(st[0] == '0' and st[1] == '6')
-            {
-                st[0] = '1';
-                st[1] = '8';
-            }
-        else if(st[0] == '0' and st[1] == '7')
-            {
-                st[0] = '1';
-                st[1] = '9';
-            }
-        else if(st[0] == '0' and st[1] == '8')
-            {
-                st[0] = '2';
-                st[1] = '0';
-            }
-        else if(st[0] == '0' and st[1] == '9')
-            {
-                st[0] = '2';
-                st[1] = '1';
-            }
-        else if(st[0] == '1' and st[1] == '0')
-            {
-                st[0] = '2';
-                st[1] = '2';
-            }
-        else if(st[0] == '1' and st[1] == '1')
-            {
-                st[0] = '2';
-                st[1] = '3';
-            }
-    }
-
+        else{
+            b=stoi(st.substr(0,2);
+            b+=12;
+            s=to_string(b);
+            st[0]=s[0];
+            st[1]=s[1];
+                   }
+                   }
+            
     for(int i=0;i<8;i++)
-        cout<<st[i];
+    cout<<st[i];
     cout<<endl;
     return 0;
 }
